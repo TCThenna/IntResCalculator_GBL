4b77dcce3ca4b4c3bada7c98ac0013226dabac2f
TCThenna <hendrik.jansen@desy.de>
2015-08-04 22:22:26 +0200

    Move constructor of AnaTel to header file
    - also change constructor to accept std::string instead of char *

--- include/AnaTel.h
+++ include/AnaTel.h
@@ -25,8 +25,6 @@
 */
 
 #include "GblTrajectory.h"
-#include "GblPoint.h"
-#include "GblData.h"
 
 #ifndef AnaTel_Included
 #define AnaTel_Included
@@ -45,7 +43,10 @@ public:
   
   //virtual ~AnaTel();
 
-  AnaTel(char * GeomFile);
+  // default constructor
+  AnaTel() = default;
+
+  AnaTel(std::string GeomFile);
   // Constructor: create telescope from geometry file
 
   AnaTel(Int_t Npl);
@@ -118,154 +119,12 @@ private:
     int GaussjSolve(double * ,double * ,int );
 
     // GBL additions
-    gbl::GblPoint* point;
+    //gbl::GblPoint* point;
     std::vector<gbl::GblPoint> listOfPoints;
 
   //ClassDef(AnaTel,1);
 
 };
-
-AnaTel::AnaTel(char * GeomFile)
-{
-  // GBL
-  //listOfPoints.reserve(3*6);
-  //TVectorD wscatSi(2);
-  //    double tetSi = 0.1;
-  //    wscatSi[0] = 1.0 / ( tetSi * tetSi ); // weight
-  //    wscatSi[1] = 1.0 / ( tetSi * tetSi );
-  //point->getPoint(150, wscat);
-
-  ifstream geometryFile;
-
-  geometryFile.open(GeomFile);
-
-  //cout << "Reading telescope geometry description from " << GeomFile << endl;
-
-  geometryFile >> _nTelPlanes  >> _iDUT ;
-
-  // !!! Important: DUT position in geometry file numbered 0...N-1 !!!
-  //     _iDUT = -1 denotes no DUT in the Setup
-
-  _planePosition   = new double[_nTelPlanes];
-  _planeThickness  = new double[_nTelPlanes];
-  _planeX0         = new double[_nTelPlanes];
-  _planeResolution = new double[_nTelPlanes];
-  _isActive        = new bool[_nTelPlanes];
-
-  // Planes have to be ordered in position along the beam line !
-  // This is not checked !!!
-
-  for(int ipl=0; ipl < _nTelPlanes; ipl++)
-    {
-      int iActive;
-      double resolution;
-
-      // All dimensions should be given in mm !!!
-
-      geometryFile >> _planePosition[ipl]
-                   >> _planeThickness[ipl]
-                   >> _planeX0[ipl]
-                   >> iActive 
-                   >> resolution ;
-
-      if(iActive)
-	{
-	  _isActive[ipl] = true ;
-	  _planeResolution[ipl]=resolution;
-	}
-      else
-	{
-          _isActive[ipl] = false ;
-          _planeResolution[ipl]=0.;
-	}
-    }
-
-  // Print out geometry information
-  //cout << "Telescope configuration with " << _nTelPlanes << " planes" << endl;
-
-
-  for(int ipl=0; ipl < _nTelPlanes; ipl++)
-    {
-      std::stringstream ss ; 
-
-      if ( ipl == _iDUT)
-	{
-        if(_isActive[ipl])
-	  ss << ipl << " : active  DUT   at" ;
-        else
-	  ss << ipl << " : passive  DUT  at" ; 
-	}
-      else
-	{
-        if(_isActive[ipl])
-	  ss << ipl << " : active  plane at" ;
-        else
-	  ss << ipl << " : passive plane at" ; 
-	}
-
-      
-      ss << "  Z [mm] = " << _planePosition[ipl] 
-	 << " dZ [um] = " << _planeThickness[ipl]*1000. ;
-      
-      if(_isActive[ipl])
-	ss << "  Res [um] = " << _planeResolution[ipl]*1000. ;
-      
-     //cout << ss.str()  << endl;
-    }
-  // Allocate arrays for track fitting
-
-  _planeDist = new double[_nTelPlanes];
-  _planeScat = new double[_nTelPlanes];
-  _tempScat1 = new double[_nTelPlanes];
-  _tempScat2 = new double[_nTelPlanes];
-  _tempScat3 = new double[_nTelPlanes];
-
-  _fitX  = new double[_nTelPlanes];
-  _nominalError  = new double[_nTelPlanes];
-
-  int arrayDim = _nTelPlanes * _nTelPlanes;
-
-  _fitArray = new double[arrayDim];
-
-
-  if(_iDUT>=0)
-    _useDUT=true;
-  else
-    _useDUT=false;
-
-  _eBeam=0.;
-  _useBeamConstraint=false ;
-}
-
-
-AnaTel::AnaTel(Int_t Npl)
-{
-  _nTelPlanes=Npl;
-
-  _planePosition   = new double[_nTelPlanes];
-  _planeThickness  = new double[_nTelPlanes];
-  _planeX0         = new double[_nTelPlanes];
-  _planeResolution = new double[_nTelPlanes];
-  _isActive        = new bool[_nTelPlanes];
-
-  _planeDist = new double[_nTelPlanes];
-  _planeScat = new double[_nTelPlanes];
-  _tempScat1 = new double[_nTelPlanes];
-  _tempScat2 = new double[_nTelPlanes];
-  _tempScat3 = new double[_nTelPlanes];
-
-  _fitX  = new double[_nTelPlanes];
-  _nominalError  = new double[_nTelPlanes];
-
-  int arrayDim = _nTelPlanes * _nTelPlanes;
-
-  _fitArray = new double[arrayDim];
-  
-  _iDUT=-1;
-  _useDUT=false;
-
-  _useBeamConstraint=false ;
-}
 
 
 #endif
--- src/AnaTel.cc
+++ src/AnaTel.cc
@@ -34,44 +34,209 @@
 #include <sstream>
 #include <cmath>
 #include "AnaTel.h"
+#include "TMatrixD.h"
 //#include "TF1.h"
 
+TMatrixD Jac5( double ds )
+{
+  /*
+     straight line, no B-field
+     track = 
+     q/p, x', y', x, y
+     0,   1,  2,  3, 4
+   */
+  TMatrixD jac(5, 5);
+  jac.UnitMatrix();
+  jac[3][1] = ds; // x = xp * ds
+  jac[4][2] = ds; // y = yp * ds
+  return jac;
+}
 
-//GblPoint getPoint(double dz, double res, TVectorD wscat, bool has_meas = true) {
-//
-//  // Propagate:
-//  TMatrixD jacPointToPoint = Jac5(dz);
-//  GblPoint point(jacPointToPoint);
-//
-//  // Add scatterer:
-//  TVectorD scat(2);
-//  scat.Zero(); // mean is zero
-//  point.addScatterer(scat, wscat);
-//
-//  // Add measurement if requested:
-//  if(has_meas) {
-//    // measurement = residual
-//    TVectorD meas(2);
-//    meas.Zero(); // ideal
-//
-//    // Precision = 1/resolution^2
-//    TVectorD measPrec(2);
-//    measPrec[0] = 1.0 / res / res;
-//    measPrec[1] = 1.0 / res / res;
-//
-//    TMatrixD proL2m(2,2);
-//    proL2m.UnitMatrix();
-// 
-//    point.addMeasurement(proL2m, meas, measPrec);
-//  }
-//
-//  return point;
-//}
-//
-//GblPoint getPoint(double dz, TVectorD wscat) {
-//  // This does not add a measurement - no resultion is given!
-//  return getPoint(dz,0.0,wscat,false);
-//}
+gbl::GblPoint getPoint(double dz, double res, TVectorD wscat, bool has_meas = true) {
+
+  // Propagate:
+  TMatrixD jacPointToPoint = Jac5(dz);
+  gbl::GblPoint point(jacPointToPoint);
+
+  // Add scatterer:
+  TVectorD scat(2);
+  scat.Zero(); // mean is zero
+  point.addScatterer(scat, wscat);
+
+  // Add measurement if requested:
+  if(has_meas) {
+    // measurement = residual
+    TVectorD meas(2);
+    meas.Zero(); // ideal
+
+    // Precision = 1/resolution^2
+    TVectorD measPrec(2);
+    measPrec[0] = 1.0 / res / res;
+    measPrec[1] = 1.0 / res / res;
+
+    TMatrixD proL2m(2,2);
+    proL2m.UnitMatrix();
+ 
+    point.addMeasurement(proL2m, meas, measPrec);
+  }
+
+  return point;
+}
+
+gbl::GblPoint getPoint(double dz, TVectorD wscat) {
+  // This does not add a measurement - no resultion is given!
+  return getPoint(dz,0.0,wscat,false);
+}
+
+AnaTel::AnaTel(std::string GeomFile)
+{
+  // GBL
+  //listOfPoints.reserve(3*6);
+  //TVectorD wscatSi(2);
+  //    double tetSi = 0.1;
+  //    wscatSi[0] = 1.0 / ( tetSi * tetSi ); // weight
+  //    wscatSi[1] = 1.0 / ( tetSi * tetSi );
+  //point->getPoint(150, wscat);
+
+  ifstream geometryFile;
+  
+  std::string help = "../geometries/";
+  help += GeomFile;
+  std::cout << " Reading geometrye file from: " << help << std::endl;
+  geometryFile.open(help.c_str());
+
+  //cout << "Reading telescope geometry description from " << GeomFile << endl;
+
+  geometryFile >> _nTelPlanes  >> _iDUT ;
+
+  // !!! Important: DUT position in geometry file numbered 0...N-1 !!!
+  //     _iDUT = -1 denotes no DUT in the Setup
+
+  _planePosition   = new double[_nTelPlanes];
+  _planeThickness  = new double[_nTelPlanes];
+  _planeX0         = new double[_nTelPlanes];
+  _planeResolution = new double[_nTelPlanes];
+  _isActive        = new bool[_nTelPlanes];
+
+  // Planes have to be ordered in position along the beam line !
+  // This is not checked !!!
+
+  std::cout << " nTelPlanes = " << _nTelPlanes << std::endl;
+
+  for(int ipl=0; ipl < _nTelPlanes; ipl++)
+    {
+      int iActive;
+      double resolution;
+
+      // All dimensions should be given in mm !!!
+
+      geometryFile >> _planePosition[ipl]
+                   >> _planeThickness[ipl]
+                   >> _planeX0[ipl]
+                   >> iActive 
+                   >> resolution ;
+
+      if(iActive)
+	{
+	  _isActive[ipl] = true ;
+	  _planeResolution[ipl]=resolution;
+	}
+      else
+	{
+          _isActive[ipl] = false ;
+          _planeResolution[ipl]=0.;
+	}
+    }
+
+  // Print out geometry information
+  //cout << "Telescope configuration with " << _nTelPlanes << " planes" << endl;
+
+
+  for(int ipl=0; ipl < _nTelPlanes; ipl++)
+    {
+      std::stringstream ss ; 
+
+      if ( ipl == _iDUT)
+	{
+        if(_isActive[ipl])
+	  ss << ipl << " : active  DUT   at" ;
+        else
+	  ss << ipl << " : passive  DUT  at" ; 
+	}
+      else
+	{
+        if(_isActive[ipl])
+	  ss << ipl << " : active  plane at" ;
+        else
+	  ss << ipl << " : passive plane at" ; 
+	}
+
+      
+      ss << "  Z [mm] = " << _planePosition[ipl] 
+	 << " dZ [um] = " << _planeThickness[ipl]*1000. ;
+      
+      if(_isActive[ipl])
+	ss << "  Res [um] = " << _planeResolution[ipl]*1000. ;
+      
+     //cout << ss.str()  << endl;
+    }
+  // Allocate arrays for track fitting
+
+  _planeDist = new double[_nTelPlanes];
+  _planeScat = new double[_nTelPlanes];
+  _tempScat1 = new double[_nTelPlanes];
+  _tempScat2 = new double[_nTelPlanes];
+  _tempScat3 = new double[_nTelPlanes];
+
+  _fitX  = new double[_nTelPlanes];
+  _nominalError  = new double[_nTelPlanes];
+
+  int arrayDim = _nTelPlanes * _nTelPlanes;
+
+  _fitArray = new double[arrayDim];
+
+
+  if(_iDUT>=0)
+    _useDUT=true;
+  else
+    _useDUT=false;
+
+  _eBeam=0.;
+  _useBeamConstraint=false ;
+
+  listOfPoints.reserve(3*6);
+
+}
+
+
+AnaTel::AnaTel(Int_t Npl)
+{
+  _nTelPlanes=Npl;
+
+  _planePosition   = new double[_nTelPlanes];
+  _planeThickness  = new double[_nTelPlanes];
+  _planeX0         = new double[_nTelPlanes];
+  _planeResolution = new double[_nTelPlanes];
+  _isActive        = new bool[_nTelPlanes];
+
+  _planeDist = new double[_nTelPlanes];
+  _planeScat = new double[_nTelPlanes];
+  _tempScat1 = new double[_nTelPlanes];
+  _tempScat2 = new double[_nTelPlanes];
+  _tempScat3 = new double[_nTelPlanes];
+
+  _fitX  = new double[_nTelPlanes];
+  _nominalError  = new double[_nTelPlanes];
+
+  int arrayDim = _nTelPlanes * _nTelPlanes;
+
+  _fitArray = new double[arrayDim];
+  
+  _iDUT=-1;
+  _useDUT=false;
+
+  _useBeamConstraint=false ;
+}
 
 
 
